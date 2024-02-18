d748fb99947f6ddc111dcf26f9a72f89487b58e7
Martin Calderon <elmcwl@gmail.com>
2024-02-17 17:46:36 -0700

    feat: paginación con SWR para posts

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Old from "./components/Old";
 import Posts from "./components/Posts";
 import Cart from "./components/Cart";
 import Products from "./components/Products";
+import PostWrapper from "./components/PostWrapper";
 
 export default function Home() {
   return (
     <div>
       {/* <Old /> */}
       {/* <Cart /> */}
-      <Products />
+      {/* <Products /> */}
       {/* <Posts /> */}
+      <PostWrapper />
     </div>
   );
 }
